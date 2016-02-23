028a83282c71fa079a033530941b9cdcdd5840bc
D. Verizhnikov <xdimedrolx@yandex.ru>
2016-02-23 05:15:29 +0300

    Changed hstore type to jsonb

--- audit.sql
+++ audit.sql
@@ -4,15 +4,13 @@
 -- This file should be generic and not depend on application roles or structures,
 -- as it's being listed here:
 --
---    https://wiki.postgresql.org/wiki/Audit_trigger_91plus    
+--    https://wiki.postgresql.org/wiki/Audit_trigger_91plus
 --
 -- This trigger was originally based on
 --   http://wiki.postgresql.org/wiki/Audit_trigger
 -- but has been completely rewritten.
 --
 -- Should really be converted into a relocatable EXTENSION, with control and upgrade files.
-
-CREATE EXTENSION IF NOT EXISTS hstore;
 
 CREATE SCHEMA audit;
 REVOKE ALL ON SCHEMA audit FROM public;
@@ -37,23 +35,23 @@ COMMENT ON SCHEMA audit IS 'Out-of-table audit/history logging tables and trigge
 -- indexes and do your analysis.
 --
 CREATE TABLE audit.logged_actions (
-    event_id bigserial primary key,
-    schema_name text not null,
-    table_name text not null,
-    relid oid not null,
-    session_user_name text,
-    action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
-    action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
-    action_tstamp_clk TIMESTAMP WITH TIME ZONE NOT NULL,
-    transaction_id bigint,
-    application_name text,
-    client_addr inet,
-    client_port integer,
-    client_query text,
-    action TEXT NOT NULL CHECK (action IN ('I','D','U', 'T')),
-    row_data hstore,
-    changed_fields hstore,
-    statement_only boolean not null
+  event_id bigserial primary key,
+  schema_name text not null,
+  table_name text not null,
+  relid oid not null,
+  session_user_name text,
+  action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
+  action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
+  action_tstamp_clk TIMESTAMP WITH TIME ZONE NOT NULL,
+  transaction_id bigint,
+  application_name text,
+  client_addr inet,
+  client_port integer,
+  client_query text,
+  action TEXT NOT NULL CHECK (action IN ('I','D','U', 'T')),
+  row_data JSONB,
+  changed_fields JSONB,
+  statement_only boolean not null
 );
 
 REVOKE ALL ON audit.logged_actions FROM public;
@@ -77,17 +75,68 @@ COMMENT ON COLUMN audit.logged_actions.row_data IS 'Record value. Null for state
 COMMENT ON COLUMN audit.logged_actions.changed_fields IS 'New values of fields changed by UPDATE. Null except for row-level UPDATE events.';
 COMMENT ON COLUMN audit.logged_actions.statement_only IS '''t'' if audit event is from an FOR EACH STATEMENT trigger, ''f'' for FOR EACH ROW';
 
-CREATE INDEX logged_actions_relid_idx ON audit.logged_actions(relid);
-CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON audit.logged_actions(action_tstamp_stm);
+CREATE INDEX logged_actions_table_id_idx ON audit.logged_actions(table_name, (row_data->>'id'));
+-- CREATE INDEX logged_actions_action_tstamp_tx_stm_idx ON audit.logged_actions(action_tstamp_stm);
 CREATE INDEX logged_actions_action_idx ON audit.logged_actions(action);
+
+CREATE OR REPLACE FUNCTION jsonb_delete_left(a jsonb, b jsonb)
+  RETURNS jsonb AS
+  $BODY$
+       SELECT COALESCE(
+              (
+              SELECT ('{' || string_agg(to_json(key) || ':' || value, ',') || '}')
+              FROM jsonb_each(a)
+              WHERE NOT ('{' || to_json(key) || ':' || value || '}')::jsonb <@ b
+              )
+       , '{}')::jsonb;
+       $BODY$
+LANGUAGE sql IMMUTABLE STRICT;
+COMMENT ON FUNCTION jsonb_delete_left(jsonb, jsonb) IS 'delete matching pairs in second argument from first argument';
+DROP OPERATOR - (jsonb, jsonb);
+CREATE OPERATOR - ( PROCEDURE = jsonb_delete_left, LEFTARG = jsonb, RIGHTARG = jsonb);
+COMMENT ON OPERATOR - (jsonb, jsonb) IS 'delete matching pairs from left operand';
+
+CREATE OR REPLACE FUNCTION jsonb_delete_left(a jsonb, b text[])
+  RETURNS jsonb AS
+  $BODY$
+       SELECT COALESCE(
+              (
+              SELECT ('{' || string_agg(to_json(key) || ':' || value, ',') || '}')
+              FROM jsonb_each(a)
+              WHERE key <> ALL(b)
+              )
+       , '{}')::jsonb;
+       $BODY$
+LANGUAGE sql IMMUTABLE STRICT;
+COMMENT ON FUNCTION jsonb_delete_left(jsonb, text[]) IS 'delete keys in second argument from first argument';
+DROP OPERATOR - (jsonb, text[]);
+CREATE OPERATOR - ( PROCEDURE = jsonb_delete_left, LEFTARG = jsonb, RIGHTARG = text[]);
+COMMENT ON OPERATOR - (jsonb, text[]) IS 'delete keys from left operand';
+
+CREATE OR REPLACE FUNCTION jsonb_delete_left(a jsonb, b text)
+  RETURNS jsonb AS
+  $BODY$
+       SELECT COALESCE(
+              (
+              SELECT ('{' || string_agg(to_json(key) || ':' || value, ',') || '}')
+              FROM jsonb_each(a)
+              WHERE key <> b
+              )
+       , '{}')::jsonb;
+       $BODY$
+LANGUAGE sql IMMUTABLE STRICT;
+COMMENT ON FUNCTION jsonb_delete_left(jsonb, text) IS 'delete key in second argument from first argument';
+DROP OPERATOR - (jsonb, text);
+CREATE OPERATOR - ( PROCEDURE = jsonb_delete_left, LEFTARG = jsonb, RIGHTARG = text);
+COMMENT ON OPERATOR - (jsonb, text) IS 'delete key from left operand';
 
 CREATE OR REPLACE FUNCTION audit.if_modified_func() RETURNS TRIGGER AS $body$
 DECLARE
     audit_row audit.logged_actions;
     include_values boolean;
     log_diffs boolean;
-    h_old hstore;
-    h_new hstore;
+    h_old jsonb;
+    h_new jsonb;
     excluded_cols text[] = ARRAY[]::text[];
 BEGIN
     IF TG_WHEN <> 'AFTER' THEN
@@ -120,18 +169,18 @@ BEGIN
     IF TG_ARGV[1] IS NOT NULL THEN
         excluded_cols = TG_ARGV[1]::text[];
     END IF;
-    
+
     IF (TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW') THEN
-        audit_row.row_data = hstore(OLD.*) - excluded_cols;
-        audit_row.changed_fields =  (hstore(NEW.*) - audit_row.row_data) - excluded_cols;
-        IF audit_row.changed_fields = hstore('') THEN
+        audit_row.row_data = row_to_json(OLD)::JSONB;
+        audit_row.changed_fields = row_to_json(NEW)::JSONB - audit_row.row_data - excluded_cols;
+        IF audit_row.changed_fields = '{}'::jsonb THEN
             -- All changed fields are ignored. Skip this update.
             RETURN NULL;
         END IF;
     ELSIF (TG_OP = 'DELETE' AND TG_LEVEL = 'ROW') THEN
-        audit_row.row_data = hstore(OLD.*) - excluded_cols;
-    ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW') THEN
-        audit_row.row_data = hstore(NEW.*) - excluded_cols;
+        audit_row.row_data = row_to_json(OLD)::JSONB - excluded_cols;
+    ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW')::JSONB THEN
+        audit_row.row_data =  row_to_json(NEW)::JSONB - excluded_cols;
     ELSIF (TG_LEVEL = 'STATEMENT' AND TG_OP IN ('INSERT','UPDATE','DELETE','TRUNCATE')) THEN
         audit_row.statement_only = 't';
     ELSE
@@ -193,8 +242,8 @@ BEGIN
         IF array_length(ignored_cols,1) > 0 THEN
             _ignored_cols_snip = ', ' || quote_literal(ignored_cols);
         END IF;
-        _q_txt = 'CREATE TRIGGER audit_trigger_row AFTER INSERT OR UPDATE OR DELETE ON ' || 
-                 quote_ident(target_table::TEXT) || 
+        _q_txt = 'CREATE TRIGGER audit_trigger_row AFTER INSERT OR UPDATE OR DELETE ON ' ||
+                 quote_ident(target_table::TEXT) ||
                  ' FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func(' ||
                  quote_literal(audit_query_text) || _ignored_cols_snip || ');';
         RAISE NOTICE '%',_q_txt;
